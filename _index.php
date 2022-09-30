9240d46da9ef53b8e69078bcf528197efd1aa59b
akhtwe <akhtwe@global-connect.asia>
2022-09-30 11:10:03 +0630

    initial test commit

---
+++ _index.php
@@ -0,0 +1,11 @@
+<?php 
+
+$siteTitle="Testing Deploy";
+?>
+<style>
+    .text-center{
+        text-align:center;
+    }
+</style>
+<h2 class="text-center">Hello, </h2>
+<h3 class="text-center"><?= $siteTitle;?></h3>
